6c32fe1d7a97abadc158b5775c3ff61c67a9b738
nth-Tung <nttung1901@gmail.com>
2026-03-20 17:19:16 +0700

    feat: implement JWT authentication and security config

---
+++ backend/src/main/java/com/hkt/fooddelivery/exception/DuplicateResourceException.java
@@ -0,0 +1,12 @@
+package com.hkt.fooddelivery.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+
+    public DuplicateResourceException(String resource, Object identifier) {
+        super(resource + " đã tồn tại với: " + identifier);
+    }
+
+}
