b320f3a8442a14beb2b60e20c545705277bec1ad
nth-Tung <nttung1901@gmail.com>
2026-03-28 21:12:01 +0700

    config: update SecurityConfig and add OpenApiConfig

--- backend/src/main/java/com/hkt/fooddelivery/security/SecurityConfig.java
+++ backend/src/main/java/com/hkt/fooddelivery/security/SecurityConfig.java
@@ -7,34 +7,68 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 // import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 // import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtAuthenticationFilter jwtAuthFilter;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,
-            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    public SecurityConfig(
+            JwtAuthenticationFilter  jwtAuthFilter,
+            JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+            CustomAccessDeniedHandler accessDeniedHandler) {
+
+        this.jwtAuthFilter = jwtAuthFilter;
         this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.accessDeniedHandler = accessDeniedHandler;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-            .csrf(csrf -> csrf.disable())
-            // .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            // .exceptionHandling(ex -> ex.authenticationEntryPoint(jwtAuthenticationEntryPoint))
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/v1/auth/**", "/h2-console/**", "/index.html", "/").permitAll().anyRequest().authenticated())
-            // .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-            .headers(headers -> headers.frameOptions(f -> f.sameOrigin()));
-
+        http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/public/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/users/**").hasRole("MERCHANT")
+                        .requestMatchers("/api/shipper/**").hasRole("SHIPPER")
+                        .requestMatchers("/api/users/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        /**
+         * Luồng hoạt động sau khi wire
+         * Request đến
+         * |
+         * JwtAuthenticationFilter
+         * |- Có Bearer token? -> nếu có thì valadate -> set SecurityContext (User đã đăng nhập)
+         * |- Nếu không có token -> bỏ qua, đi tiếp
+         * |
+         * authorizeHttpRequests kiểm tra
+         * |- /api/auth/** -> permitAll -> cho qua
+         * |- /api/admin/** -> hasRole("ADMIN")
+         *      |- Chưa đăng nhập -> jwtAuthenticationEntryPoint -> 401
+         *      |- Đã đăng nhập nhưng role USER -> CustomAccessDeniedHandler -> 403
+         * |- /api/users/** -> hasAllRoles("ADMIN", "USER")
+         *      |- Chưa đăng nhập -> jwtAuthenticationEntryPoint -> 401
+         * |- anyRequest().authenticated()
+         *      |- Chưa đăng nhập -> jwtAuthenticationEntryPoint -> 401
+         */
         return http.build();
     }
 
